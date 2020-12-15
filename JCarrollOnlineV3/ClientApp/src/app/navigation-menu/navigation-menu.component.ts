e03f64698d635798a404ee82437c934250cca5ab
John Carroll <john.carroll@vivomi.com>
2020-12-15 12:49:55 -0800

    remove .vs dependencies and hide the nav menu bar on the splash page

--- JCarrollOnlineV3/ClientApp/src/app/navigation-menu/navigation-menu.component.ts
+++ JCarrollOnlineV3/ClientApp/src/app/navigation-menu/navigation-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { AuthorizeService } from '../../api-authorization/authorize.service';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation-menu',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-menu.component.scss']
 })
 export class NavigationMenuComponent implements OnInit {
+  public currentUrl: string;
 
-  constructor() { }
+  constructor(private authorizeService: AuthorizeService, public router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(data => {
+        this.currentUrl = (data as NavigationEnd).url;
+      });
+  }
 
   ngOnInit(): void {
   }
